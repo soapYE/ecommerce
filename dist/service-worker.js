db028bcfca3c6f5f99a39c175c9ab7bf7d16c277
soapYE <1614744147@qq.com>
2020-07-22 11:36:03 +0800

    solve white page problem

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.74a90dd57e79c85c715b0bbd4683b60c.js", "/workbox-v4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.5757e4740ca594eff2ca9e3475a285db.js", "/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings()
